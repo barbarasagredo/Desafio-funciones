c657d6c323390e9e04da8659be173c97e29a0909
Bárbara Sagredo <barpaz.sc@gmail.com>
2025-09-07 18:41:44 -0300

    first commit

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,9 @@
+//DECLARACION DE FUNCION
+function example(a, b, c) {
+  return a + b + c;
+}
+
+//FUNCION DE EXPRESION
+example = function (a, b, c) {
+  return a + b + c;
+};
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,7 @@
+//FUNCION DE EXPRESION
+let suma = function (a, b) {
+  return a + b;
+};
+
+// ARROW FUNCTION
+suma = (a, b) => a + b;
