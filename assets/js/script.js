d59544c66f7b64419b4c2088f09df8cf2e04fc10
Bárbara Sagredo <barpaz.sc@gmail.com>
2025-09-07 19:09:14 -0300

    css agregado y se aplicaron estilos a la página de pintar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-function pintar(){
-ele.style.backgroundColor = 'yellow'
+// function pintar(){
+// ele.style.backgroundColor = 'yellow'
+// }
+// const ele = document.getElementById("ele1")
+// ele.addEventListener("click", pintar);
+
+function pintar(element, color = "green", fontColor = "white") {
+  element.style.backgroundColor = color;
+  element.style.color = fontColor;
 }
-const ele = document.getElementById("ele1")
-ele.addEventListener("click", pintar);
+
+const ele = document.getElementById("ele1");
+
+pintar(ele);
+
+ele.addEventListener("click", function (event) {
+  pintar(event.target, "yellow", "black");
+});
